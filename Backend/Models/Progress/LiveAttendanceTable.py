ad942e27f400301c24e6bc87039f8182cb26d527
Parthasarathy G <parthasarathy.g@codework.ai>
2026-04-04 12:53:40 +0530

    feat: add detailed attendance tracking fields to live attendance schema and generate migration

--- Backend/Models/Progress/LiveAttendanceTable.py
+++ Backend/Models/Progress/LiveAttendanceTable.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Boolean, Column,String,BigInteger,DateTime,Date,func,ForeignKey,Integer,Boolean
+from sqlalchemy import Column, String, BigInteger, DateTime, Date, func, ForeignKey, Integer, Boolean
 from Models.User_Tables.User_Profile import Base
 
 class LiveAttendanceTable(Base):
@@ -8,6 +8,11 @@ class LiveAttendanceTable(Base):
     User_ID = Column(String(50),ForeignKey("user_profile.user_id"),nullable=False)
     Live_Class_ID = Column(String(50),ForeignKey("live_course_table.Live_ID"),nullable=False,)
     Module_ID = Column(String(50),ForeignKey("course_module_table.Module_ID"),nullable=False)
+    
+    Attended_Live = Column(Boolean, default=False)
+    Watched_Recording = Column(Boolean, default=False)
+    Is_Present = Column(Boolean, default=False)
+    
     Completed_At = Column(DateTime)
     updated_at = Column(DateTime, default=func.now(), onupdate=func.now())
     created_at = Column(DateTime, default=func.now())
